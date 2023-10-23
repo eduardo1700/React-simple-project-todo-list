3f3d232f76eee9cdcacad7dfdb412994aab3237d
eduardo1700 <edu.alex.mp@gmail.com>
2023-10-23 20:47:00 +0100

    submit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
+import { useState } from "react"
 import "./styles.css"
 
 export default function App(){
+  const [newItem, setNewItem]= useState("")
+  const [todos, setTodos] = useState([])
+
+  function handleSubmit(e){
+    e.preventDefault()
+    setTodos((currentTodos)=>{
+      return[
+        ...currentTodos,
+        {id:crypto.randomUUID(), title: newItem, completed:false},
+      ]
+    })
+  }
   return (
     <>
-  <form className="new-item-form">
+  <form onSubmit={handleSubmit} className="new-item-form">
     <div className="form-row">
       <label htmlFor="item">New Item</label>
-      <input type="text" id="item"/>
+      <input value={newItem} onChange={e => setNewItem(e.target.value)}type="text" id="item"/>
     </div>
     <button className="btn">Add</button>
   </form>
