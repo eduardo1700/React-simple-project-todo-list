a9b1b669a370e796fb08a91ca2f50673a4a85803
eduardo1700 <edu.alex.mp@gmail.com>
2023-10-23 20:25:45 +0100

    Continuação

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,22 @@ export default function App(){
     <button className="btn">Add</button>
   </form>
   <h1 className="header">Todo list</h1>
-  <ul className="list"
+  <ul className="list">
+    <li>
+      <label>
+        <input type="checkbox"/>
+        Item 1
+      </label>
+      <button className="btn btn-danger">Delete</button>
+    </li>
+    <li>
+      <label>
+        <input type="checkbox"/>
+        Item 2
+      </label>
+      <button className="btn btn-danger">Delete</button>
+    </li>
+  </ul>
   </>
   )
 }
